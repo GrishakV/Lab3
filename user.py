8ea0d4567e1960c999909551ebf97f81c17ab18f
GrishakV <vovan095@gmail.com>
2020-09-24 21:43:59 +0300

    Добавлена программа запроса вседений о польователе

---
+++ user.py
@@ -0,0 +1,6 @@
+# Запрашиваем имя, возраст, место жительства. Затем выводим их.
+nameUser = input("What is your name?")
+oldUser = input("How old are you?")
+locationUser = input("Were are you live?")
+
+print("This is {0}.\nIt is {1}.\n(S)he live in {2}. ".format(nameUser, oldUser, locationUser))
